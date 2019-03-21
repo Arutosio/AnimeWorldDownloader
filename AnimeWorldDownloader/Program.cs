1b17a1cf0eb0b0df8e38729f04abb73262ab0d41
Arutosio <Arutosio@outlook.com>
2019-03-22 00:40:55 +0100

    2.8
    Miglioramento della console

--- AnimeWorldDownloader/Program.cs
+++ AnimeWorldDownloader/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.IO;
-using static System.Net.Mime.MediaTypeNames;
+using AnimeWorldDownloader.TerminalColor;
 
 namespace AnimeWorldDownloader
 {
@@ -9,107 +9,81 @@ namespace AnimeWorldDownloader
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("{0:0.00}%", PersentageCalculation(103.7, 253.3));
             /*Declaration Fase */
             string pLink, link, nFile, replace, wRepla, path = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            path = path.Replace(path.Split('\\')[path.Split('\\').Length -1],"");
+            path = path.Replace(path.Split('\\')[path.Split('\\').Length - 1], "");
             int riprendiDalla = 0, nEpisodi = 0;
-            /*Preparation Fase*/
+            /*Preparation FASE*/
             Console.WriteLine("--- <-_ Benvenuti su AnimeWorldDownloader by Arutosio - Testo a cura di Jamlegend _-> ---");
-            Console.Write("        ~  "); Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("Per informazione consultare la pagina GitHub della repository");
-            Console.ResetColor(); Console.WriteLine("  ~       ");
-            Console.ResetColor();
-            Console.Write("Path: "); Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine(path); Console.ResetColor();
-            LineFase("Inizio fase PREPARATORIA");
-            pLink = GetPLink();
-            nFile = GetFileName(pLink);
-            replace = GetRemplaceChange();
-            nEpisodi = GetNEpisodi();
-            if (IsRipresa()) { riprendiDalla = GetNERiprendere(); }
-            /*Star proces Fase*/
-            LineFase("Inizio fase SCARICAMENTO!");
-            CreateFolder(path, nFile.Split('_')[0]);
-            for (int i = riprendiDalla; nEpisodi >= i; i++)
+            Console.Write("        ~  "); CColor.WriteC("Per informazione consultare la pagina GitHub della repository", "cyan"); Console.WriteLine("  ~       ");
+
+            Console.Write("Path: "); CColor.WriteC(path,"yellow");
+            do
             {
-                wRepla = i < 10 ? "0" + i : "" + i;
-                link = pLink.Replace(replace, wRepla);
                 Console.WriteLine();
-                Console.Write("GET> ");
-                Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine(link); ; Console.ResetColor();
-                DoAGetRequest(link, path+ nFile.Split('_')[0]+@"\"+nFile.Replace(replace, wRepla));
-            }
-            Console.Write("\r\n======"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("PROCESSO COMPLETATO!"); Console.ResetColor(); Console.WriteLine("====== premi un tasto per chiudere il programma.");
-            Console.ReadKey();
+                LineFase("Inizio fase PREPARATORIA");
+
+                Console.WriteLine("--- Inserisci l'URL diretto dell'episodio da Scaricare: ");
+                pLink = CColor.ReadLineC("cyan");
+
+                nFile = pLink.Split('/')[pLink.Split('/').Length - 1];
+
+                Console.Write("--- Inserisci il Numero dell'episodio tratto dall'URL (ES:00, 04, 11): ");
+                replace = CColor.ReadLineC("Yellow");
+
+                nEpisodi = GetNumberOfC("--- Inserisci il numero di episodi: ", "yellow");
+
+                if (IsRipresa()) { riprendiDalla = GetNumberOfC("--- Inserisci il numero dell'episodio da cui vuoi riprendere a scaricare: ", "yellow"); }
+
+                /*Procces FASE*/
+                LineFase("Inizio fase SCARICAMENTO!");
+
+                CreateFolder(path, nFile.Split('_')[0]);
+                for (int i = riprendiDalla; nEpisodi >= i; i++)
+                {
+                    wRepla = i < 10 ? "0" + i : "" + i;
+                    link = pLink.Replace(replace, wRepla);
+                    Console.Write("GET> "); CColor.WriteLineC(link,"cyan");
+                    DoAGetRequest(link, path + nFile.Split('_')[0] + @"\" + nFile.Replace(replace, wRepla));
+                }
+                Console.Write("======"); CColor.WriteC("PROCESSO CONCLUSO!","green"); Console.WriteLine("======");
+                Console.Write("Premi "); CColor.WriteC("Y", "green"); Console.Write(" se vuoi scaricare un'altro anime, altrimenti premi un altro tasto per "); CColor.WriteC("USCIRE", "red"); Console.Write(": ");
+            } while (Console.ReadKey().KeyChar.ToString().ToLower().Equals("y"));
         }
-        public static void LineFase(String text)
+        ///STATIC Method!
+        public static double PersentageCalculation(double current, double maximum)
         {
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.Write("::::::::::::::::::::::"); Console.ForegroundColor = ConsoleColor.Black;
-            Console.Write(text); Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("::::::::::::::::::::::\r\n"); Console.ResetColor();
+            return (current / maximum) * 100;
         }
-        public static string GetPLink()
+        public static void LineFase(string text)
         {
-            Console.Write("--- Inserisci l'URL diretto dell'episodio da Scaricare: ");
-            string res;
-            Console.ForegroundColor = ConsoleColor.Cyan; res = Console.ReadLine(); Console.ResetColor();
-            return res;
+            Console.BackgroundColor = ConsoleColor.White; Console.Write("::::::::::::::::::::::");
+            CColor.WriteC(text, "black");
+            Console.BackgroundColor = ConsoleColor.White;  Console.WriteLine("::::::::::::::::::::::\r\n");
+            Console.ResetColor();
         }
-        public static string GetFileName(string pLink)
+        public static int GetNumberOfC(string print, string color)
         {
-            return pLink.Split('/')[pLink.Split('/').Length - 1];
-        }
-        public static string GetRemplaceChange()
-        {
-            string res;
-            Console.Write("--- Inserisci il Numero dell'episodio tratto dall'URL (ES:00, 04, 11): ");
-            Console.ForegroundColor = ConsoleColor.Yellow; res = Console.ReadLine(); Console.ResetColor();
-            return res;
-        }
-        public static int GetNEpisodi()
-        {
+            Console.Write(print);
             bool repeatC = true;
             int res = 0;
             do
             {
-                Console.Write("--- Inserisci il numero di episodi: "); Console.ForegroundColor = ConsoleColor.Yellow;
-                try { res = Convert.ToInt32(Console.ReadLine()); Console.ResetColor(); repeatC = false; }
-                catch { Console.ResetColor(); Console.WriteLine("--- Non hai inserito un numero valido"); }
+                try { res = Convert.ToInt32(CColor.ReadLineC(color)); Console.ResetColor(); repeatC = false; }
+                catch { Console.WriteLine("--- Non hai inserito un numero valido, Riprova: "); }
             } while (repeatC);
             return res;
         }
         public static bool IsRipresa()
         {
-            Console.Write("--- Devi riprendere da una certa puntata?["); Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("y"); Console.ResetColor(); Console.Write("= yes]: "); Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("--- Devi riprendere da una certa puntata? ["); CColor.WriteC("Y", "Green"); Console.Write(" = "); CColor.WriteC("Yes", "green");  Console.Write("]: ");
             if (Console.ReadKey().KeyChar.ToString().ToLower().Equals("y"))
             {
-                Console.ResetColor();
                 Console.WriteLine();
                 return true;
             }
-            Console.ResetColor();
             return false;
-        }
-        public static int GetNERiprendere()
-        {
-            bool repeatC = true;
-            int res = 0;
-            do
-            {
-                Console.Write("--- Inserisci il numero dell'episodio da cui vuoi riprendere a scaricare: "); Console.ForegroundColor = ConsoleColor.Yellow;
-                try { res = Convert.ToInt32(Console.ReadLine()); repeatC = false; }
-                catch { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("--- Non hai inserito un numero valido"); }
-                Console.ResetColor();
-            } while (repeatC);
-            return res;
-        }
-        public static string GetFolderName(string pLink)
-        {
-            string res = pLink.Split('/')[pLink.Split('/').Length - 1].Split('_')[0];
-            Console.Write("Imposto per la creazione della cartella..  "); Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(res); Console.ResetColor();
-            return res;
         }
         public static void CreateFolder(string path, string name)
         {
@@ -118,13 +92,13 @@ namespace AnimeWorldDownloader
                 // Determine whether the directory exists.
                 if (!Directory.Exists(path+name))
                 {
-                    Console.WriteLine("Verrà creata una cartella con il nome: " + name);
+                    Console.Write("Verrà creata una cartella con il nome: "); CColor.WriteLineC(name, "yellow");
                 } else return;
 
                 // Try to create the directory.
                 DirectoryInfo di = Directory.CreateDirectory(path+name);
-                Console.Write("La cartella e stata creata con successo. "); Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine(Directory.GetCreationTime(path+name)); Console.ResetColor();
+                Console.Write("La cartella e stata creata con successo il ");
+                CColor.WriteLineC(Directory.GetCreationTime(path+name).ToString(),"magenta"); 
 
                 // Delete the directory.
                 //di.Delete();
@@ -141,14 +115,14 @@ namespace AnimeWorldDownloader
         public static void DoAGetRequest(string link, string nFileFix)
         {
             // Construct HTTP request to get the file
-            Console.Write("--- Dowloading: "); Console.ForegroundColor = ConsoleColor.Yellow;  Console.WriteLine(GetFileName(link)); Console.ResetColor();
+            Console.Write("--- Dowloading: "); CColor.WriteLineC(link.Split('/')[link.Split('/').Length - 1], "yellow");
             try
             {
                 using (var client = new WebClient())
                 { client.DownloadFile(link, nFileFix); }
-                Console.Write("   ===> "); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("DONE!"); Console.ResetColor();
+                Console.Write("   ===> "); CColor.WriteLineC("DONE! \r\n", "green");
             }
-            catch { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("   ### Error ###"); Console.ResetColor(); }
+            catch { CColor.WriteLineC("   ### Error ### \r\n", "red"); }
         }
     }
 }
---
+++ AnimeWorldDownloader/TerminalColor/CColor.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeWorldDownloader.TerminalColor
+{
+    public static class CColor
+    {
+        public static void WriteC(string write, string color)
+        {
+            switch (color.ToLower())
+            {
+                case "black":
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "darkblue":
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "darkgreen":
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "darkcyan":
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "darkred":
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "darkmagenta":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "darkyellow":
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "darkgray":
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "gray":
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "cyan":
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "magenta":
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                case "white":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(write); Console.ResetColor();
+                    break;
+                default:
+                    Console.Write(write); Console.Write("(Error:ForegroundColor " + '"' + (color != null ? color + '"' + "not found." : "you did not choose a color"));
+                    break;
+            }
+        }
+        public static void WriteLineC(string write, string color)
+        {
+            switch (color.ToLower())
+            {
+                case "black":
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkblue":
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkgreen":
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkcyan":
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkred":
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkmagenta":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkyellow":
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkgray":
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "gray":
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "cyan":
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "magenta":
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "white":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                default:
+                    Console.Write(write); Console.Write("(Error:ForegroundColor " + '"' + (color != null ? color + '"' + "not found." : "you did not choose a color"));
+                    break;
+            }
+        }
+        public static void BackgroundC(string write, string color)
+        {
+            switch (color.ToLower())
+            {
+                case "black":
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkblue":
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkgreen":
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkcyan":
+                    Console.BackgroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkred":
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkmagenta":
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkyellow":
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "darkgray":
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "gray":
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "blue":
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "magenta":
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "yellow":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                case "white":
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.WriteLine(write); Console.ResetColor();
+                    break;
+                default:
+                    Console.Write(write); Console.Write("(Error:BackgroundColor " + '"' + (color != null ? color + '"' + "not found." : "you did not choose a color"));
+                    break;
+            }
+        }
+        public static string ReadLineC(string color)
+        {
+            switch (color.ToLower())
+            {
+                case "black":
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case "darkblue":
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    break;
+                case "darkgreen":
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case "darkcyan":
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    break;
+                case "darkred":
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case "darkmagenta":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    break;
+                case "darkyellow":
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case "darkgray":
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case "gray":
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "cyan":
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "magenta":
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case "yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case "white":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                default:
+                    Console.WriteLine("(Error:ForegroundColor " + '"' + (color != null ? color + '"' + "not found." : "you did not choose a color"));
+                    break;
+            }
+            string res = Console.ReadLine();
+            Console.ResetColor();
+            return res;
+        }
+    }
+}
+
