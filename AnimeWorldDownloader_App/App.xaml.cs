4f0c28a45af7049adbbea17e430f70357d68f265
Arutosio <30377429+Arutosio@users.noreply.github.com>
2023-04-18 13:20:24 +0200

    new Maui Project

---
+++ AnimeWorldDownloader_App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AnimeWorldDownloader_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
